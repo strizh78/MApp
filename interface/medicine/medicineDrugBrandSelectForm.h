c9c08ac2cc8a7ff6660c1d71a56a70122f7d2285
rust379 <mansurov.rust@yandex.ru>
2021-02-18 17:53:32 +0300

    Add form for adding brand names

---
+++ interface/medicine/medicineDrugBrandSelectForm.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QDialog>
+#include <QLabel>
+#include <QStringListModel>
+
+#include <memory>
+
+namespace Ui {
+class MedicineDrugBrandSelectForm;
+}
+
+class MedicineDrugBrandSelectForm : public QDialog
+{
+    Q_OBJECT
+
+public:
+     MedicineDrugBrandSelectForm(const std::vector<QString>& brands,
+                                 QLabel* label,
+                                 QWidget *parent = nullptr);
+    ~MedicineDrugBrandSelectForm();
+
+signals:
+    void brandsSelectedSignal(QLabel* label, std::vector<QString> data);
+
+private slots:
+     void on_addButton_clicked();
+     void on_deleteButton_clicked();
+     void on_editButton_clicked();
+     void on_listView_doubleClicked(const QModelIndex &index);
+     void on_buttonBox_accepted();
+     void on_buttonBox_rejected();
+
+private:
+    Ui::MedicineDrugBrandSelectForm *ui;
+    QLabel* label_;
+    std::shared_ptr<QStringListModel> listModel;
+};
+
