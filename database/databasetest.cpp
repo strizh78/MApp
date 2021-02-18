59dc3c0185bd12c14362e8c088e35b97b975df98
rust379 <mansurov.rust@yandex.ru>
2021-02-19 01:27:50 +0300

    change ctatic const to const

--- database/databasetest.cpp
+++ database/databasetest.cpp
@@ -10,19 +10,19 @@ std::vector<medicine::Drug> DatabaseTest::medicinesList_ = DatabaseTest::initMed
 void DatabaseTest::homeopathyDrugs(std::vector<homeopathy::Drug>& receiver) {
     using namespace homeopathy;
 
-    static const std::vector<Dilutions> d1 = {
+    const std::vector<Dilutions> d1 = {
         Dilutions::C30,
         Dilutions::C200,
         Dilutions::C1000};
-    static const std::vector<Dilutions> d2 = {
+    const std::vector<Dilutions> d2 = {
         Dilutions::C30,
         Dilutions::C1000,
         Dilutions::LM1};
-    static const std::vector<Dilutions> d3 = {
+    const std::vector<Dilutions> d3 = {
         Dilutions::M1,
         Dilutions::LM6,
         Dilutions::C12};
-    static const std::vector<Dilutions> d4 = {
+    const std::vector<Dilutions> d4 = {
         Dilutions::M1,
         Dilutions::LM6,
         Dilutions::M10,
@@ -43,19 +43,19 @@ void DatabaseTest::homeopathyDrugs(std::vector<homeopathy::Drug>& receiver) {
 void DatabaseTest::availableHomeopathyDrugs(std::vector<homeopathy::Drug>& receiver) {
     using namespace homeopathy;
 
-    static const std::vector<Dilutions> d1 = {
+    const std::vector<Dilutions> d1 = {
         Dilutions::C30,
         Dilutions::C200,
         Dilutions::C1000};
-    static const std::vector<Dilutions> d2 = {
+    const std::vector<Dilutions> d2 = {
         Dilutions::C30,
         Dilutions::C1000,
         Dilutions::LM1};
-    static const std::vector<Dilutions> d3 = {
+    const std::vector<Dilutions> d3 = {
         Dilutions::M1,
         Dilutions::LM6,
         Dilutions::C12};
-    static const std::vector<Dilutions> d4 = {
+    const std::vector<Dilutions> d4 = {
         Dilutions::M1,
         Dilutions::LM6,
         Dilutions::M10,
@@ -75,47 +75,47 @@ void DatabaseTest::availableHomeopathyDrugs(std::vector<homeopathy::Drug>& recei
 const std::vector<medicine::Drug> DatabaseTest::initMedicineDrugs() {
     using namespace medicine;
 
-    static auto allReleaseForms = releaseForms();
-    static const std::vector<ReleaseForm> relForms1 = {
+    auto allReleaseForms = releaseForms();
+    const std::vector<ReleaseForm> relForms1 = {
         allReleaseForms[0],
         allReleaseForms[1],
         allReleaseForms[2]};
-    static const std::vector<ReleaseForm> relForms2 = {
+    const std::vector<ReleaseForm> relForms2 = {
         allReleaseForms[2],
         allReleaseForms[5],
         allReleaseForms[1]};
-    static const std::vector<ReleaseForm> relForms3 = {
+    const std::vector<ReleaseForm> relForms3 = {
         allReleaseForms[6],
         allReleaseForms[7],
         allReleaseForms[8]};
-    static const std::vector<ReleaseForm> relForms4 = {
+    const std::vector<ReleaseForm> relForms4 = {
         allReleaseForms[4],
         allReleaseForms[9],
         allReleaseForms[10]};
-    static const std::vector<QString> brands1 = {
+    const std::vector<QString> brands1 = {
         "brandA",
         "brandB",
         "brandC"};
-    static const std::vector<QString> brands2 = {
+    const std::vector<QString> brands2 = {
         "brandA"};
-    static const std::vector<QString> brands3 = {
+    const std::vector<QString> brands3 = {
         "brandB",
         "brandD"};
-    static const std::vector<QString> dosages1 = {
+    const std::vector<QString> dosages1 = {
         "10-12, dos1",
         "12-16, dos2",
         "16+ dos3"};
-    static const std::vector<QString> dosages2 = {
+    const std::vector<QString> dosages2 = {
         "1-12 мес., dos3",
         "1-10, dos4",
         "10-18+ dos5"};
-    static const std::vector<QString> dosages3 = {
+    const std::vector<QString> dosages3 = {
         "2-3, dos6",
         "3-4, dos7",
         "4-16 dos8"};
-    static const std::vector<QString> dosages4 = {
+    const std::vector<QString> dosages4 = {
         "dos9"};
-    static const std::vector<medicine::Drug> medicines = {
+    const std::vector<medicine::Drug> medicines = {
         Drug("actSubsA", "actSubsLatA", false, relForms1, brands1, dosages1, 3.14),
         Drug("actSubsB", "", false, relForms2, brands1, dosages1, 2.71),
         Drug("actSubsC", "actSubsLatC", false, relForms3, brands2, dosages2, 60.22),
