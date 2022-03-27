6938b059d25b8eefd5e42eba71a02e1884a6f65f
Khaykova Svetlana <38105548+strizh78@users.noreply.github.com>
2022-03-27 22:06:48 +0300

    Windows scaling fix (#149)

--- main.cpp
+++ main.cpp
@@ -4,11 +4,26 @@
 
 #include <QApplication>
 #include <QTranslator>
+#include <QGuiApplication>
+#include <QCoreApplication>
 
 #include <memory>
 
-int main(int argc, char *argv[])
-{
+void setupScaleDpiUiFlags() {
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QApplication::setAttribute(Qt::AA_Use96Dpi);
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
+
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1.5");
+    qputenv("QT_ENABLE_HIGHDPI_SCALING", "1");
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+}
+
+int main(int argc, char *argv[]) {
+    setupScaleDpiUiFlags();
+
     DatabasePtr database = std::make_shared<DatabaseTest>();
 
     QApplication a(argc, argv);
